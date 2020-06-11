0b653cc1caf52055df3e1e8aac34205e2e5e38c5
iankbovard <iankbovard@gmail.com>
2020-06-11 18:55:36 -0500

    highscore scene resized

--- src/pages/HackShackAttack/scenes/PreloaderScene.js
+++ src/pages/HackShackAttack/scenes/PreloaderScene.js
@@ -290,7 +290,7 @@ export default class PreloaderScene extends Phaser.Scene {
   ready() {
     this.readyCount += 1;
     if (this.readyCount === 2) {
-      this.scene.start('BackToTitle');
+      this.scene.start('HighScore');
     }
   }
 }
