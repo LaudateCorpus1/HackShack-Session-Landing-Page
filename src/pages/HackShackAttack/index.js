d74e11cce3355851ae0514a8c63f5dd9a4587ca3
iankbovard <iankbovard@gmail.com>
2020-06-11 20:36:22 -0500

    changed image paths removed old assets and scenes

--- src/pages/HackShackAttack/index.js
+++ src/pages/HackShackAttack/index.js
@@ -7,7 +7,6 @@ import Phaser from 'phaser';
 import BootScene from './scenes/BootScene';
 import PreloaderScene from './scenes/PreloaderScene';
 import TitleScene from './scenes/TitleScene';
-import AttractModeScene from './scenes/AttractModeScene';
 import GameScene from './scenes/GameScene';
 import GameOverScene from './scenes/GameOverScene';
 import HighScoreScene from './scenes/HighScoreScene';
@@ -22,7 +21,6 @@ class Game extends Phaser.Scene {
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
-    this.scene.add('AttractMode', AttractModeScene);
     this.scene.add('Game', GameScene);
     this.scene.add('GameOver', GameOverScene);
     this.scene.add('HighScore', HighScoreScene);
