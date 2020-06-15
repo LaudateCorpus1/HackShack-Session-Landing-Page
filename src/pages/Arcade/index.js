a43b8290e130a3ab3d75b8499e55d619778ff18f
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-06-15 10:36:36 -0600

    use heading in pages

--- src/pages/Arcade/index.js
+++ src/pages/Arcade/index.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import { Heading } from 'grommet';
 import { Layout, Card, CardGrid } from '../../components/index';
 import { arcadeContent } from '../../data/CardData/PageContent';
-import { MainTitle } from '../Schedule/styles';
+import { PageHeader } from '../../components/PageHeading';
 
 const Arcade = () => {
   return (
     <Layout background="/img/arcade-background.jpg">
-      <MainTitle>
-        <Heading color="text-strong" margin={{ bottom: 'large', top: 'none' }}>
-          ARCADE
-        </Heading>
-      </MainTitle>
-      <CardGrid>
-        {arcadeContent.map(content => (
-          <Card
-            title={content.title}
-            alt={content.alt}
-            background={content.background}
-            image={content.image}
-            desc={content.desc}
-            label={content.label}
-            link={content.link}
-            path={content.path}
-          />
-        ))}
-      </CardGrid>
+      <PageHeader title="ARCADE">
+        <CardGrid>
+          {arcadeContent.map(content => (
+            <Card
+              title={content.title}
+              alt={content.alt}
+              background={content.background}
+              image={content.image}
+              desc={content.desc}
+              label={content.label}
+              link={content.link}
+              path={content.path}
+            />
+          ))}
+        </CardGrid>
+      </PageHeader>
     </Layout>
   );
 };
