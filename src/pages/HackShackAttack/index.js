53c7b85e54dbb79b39599550cded439c47bdfdc2
iankbovard <iankbovard@gmail.com>
2020-06-17 18:23:11 -0500

    terms and conditions layer added

--- src/pages/HackShackAttack/index.js
+++ src/pages/HackShackAttack/index.js
@@ -1,8 +1,8 @@
 /* (C) Copyright 2019 Hewlett Packard Enterprise Development LP. */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IonPhaser } from '@ion-phaser/react';
 import styled from 'styled-components';
-import { Box, Button } from 'grommet';
+import { Box, Button, Layer, Image, Text, Anchor, CheckBox } from 'grommet';
 import { Link } from 'react-router-dom';
 import { FormPreviousLink } from 'grommet-icons';
 import Phaser from 'phaser';
@@ -39,21 +39,125 @@ class Game extends Phaser.Scene {
   }
 }
 
+const gameConfig = {
+  initialize: true,
+  game: {
+    width: 1366,
+    height: 768,
+    parent: 'phaser-game',
+    type: Phaser.AUTO,
+    input: {
+      gamepad: true,
+      queue: true,
+    },
+    scale: {
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      mode: Phaser.Scale.ENVELOP,
+    },
+    pixelArt: true,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: false,
+        gravity: { y: 0 },
+      },
+    },
+    scene: Game,
+  },
+};
+
 const GameContainer = styled(Box)`
   position: relative;
   min-height: 1100px;
 `;
+
 const BackgroundWrapper = styled(Box)`
   position: absolute;
   z-index: 10;
 `;
 
-const HackShackAttack = () => {
-  const preventScroll = e => {
-    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-      e.preventDefault();
+const preventScroll = e => {
+  if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    e.preventDefault();
+  }
+};
+const TermsLayer = ({ setAccepted }) => {
+  const [checked, setChecked] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSubmit = () => {
+    if (checked) {
+      setAccepted(true);
+      setError(false);
+    } else {
+      setError(true);
     }
   };
+
+  return (
+    <Layer>
+      <Box alignSelf="end" pad="small">
+        <Link to={{ pathname: '/arcade' }}>
+          <Button icon={<FormPreviousLink />} label="Back to Arcade" />
+        </Link>
+      </Box>
+      <Box direction="column" pad="large" gap="medium" align="start">
+        <Image
+          width="100%"
+          fit="cover"
+          alt="attack marquee"
+          src="/assets/attack-marquee.png"
+        />
+        <Text style={{ fontFamily: 'Kemco' }} size="xxlarge" color="#ffffff">
+          By Playing in the Hack Shack Attack contest you agree to signing your
+          life over to HPE.
+        </Text>
+        <Text color="#ffffff">
+          Read the full{' '}
+          <Anchor href="#" target="_blank" rel="noreferrer noopener">
+            <Text color="#ffffff" weight={900}>
+              Terms
+            </Text>
+          </Anchor>{' '}
+          and{' '}
+          <Anchor href="#" target="_blank" rel="noreferrer noopener">
+            <Text color="#ffffff" weight={900}>
+              Conditions
+            </Text>
+          </Anchor>{' '}
+          that apply.
+        </Text>
+        <Box direction="row" align="center">
+          <CheckBox
+            onChange={() => {
+              setChecked(!checked);
+              setError(false);
+            }}
+          />
+          <Text color="#ffffff" style={{ fontFamily: 'Kemco' }}>
+            I agree to the data that you're collecting.
+          </Text>
+        </Box>
+        {error && <Text>You must agree to play.</Text>}
+        <Button onClick={() => handleSubmit()}>
+          <Box background="#00567acc">
+            <Text
+              color="#ffffff"
+              style={{ fontFamily: 'Kemco' }}
+              size="xxlarge"
+              margin="medium"
+            >
+              Start Game
+            </Text>
+          </Box>
+        </Button>
+      </Box>
+    </Layer>
+  );
+};
+const HackShackAttack = () => {
+  const { game, initialize } = gameConfig;
+  const [accepted, setAccepted] = useState(false);
 
   useEffect(() => {
     window.addEventListener('keydown', preventScroll);
@@ -61,34 +165,7 @@ const HackShackAttack = () => {
       window.removeEventListener('keydown', preventScroll);
     };
   }, []);
-  const gameConfig = {
-    initialize: true,
-    game: {
-      width: 1366,
-      height: 768,
-      parent: 'phaser-game',
-      type: Phaser.AUTO,
-      input: {
-        gamepad: true,
-        queue: true,
-      },
-      scale: {
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        mode: Phaser.Scale.ENVELOP,
-      },
-      pixelArt: true,
-      physics: {
-        default: 'arcade',
-        arcade: {
-          debug: false,
-          gravity: { y: 0 },
-        },
-      },
-      scene: Game,
-    },
-  };
 
-  const { game, initialize } = gameConfig;
   return (
     <GameContainer fill>
       <BackgroundWrapper
@@ -105,9 +182,12 @@ const HackShackAttack = () => {
           </Link>
         </Box>
       </BackgroundWrapper>
-      <Box fill id="phaser-game">
-        <IonPhaser game={game} initialize={initialize} />
-      </Box>
+      {accepted && (
+        <Box fill id="phaser-game">
+          <IonPhaser game={game} initialize={initialize} />
+        </Box>
+      )}
+      {!accepted && <TermsLayer setAccepted={setAccepted} />}
     </GameContainer>
   );
 };
