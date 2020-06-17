2f93cd22334d504e99366acfb086454c7e2c42d6
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-06-17 16:17:43 -0600

    add all the stickers to file

--- src/pages/StickerWall/stickers.js
+++ src/pages/StickerWall/stickers.js
@@ -3,48 +3,162 @@ import { Monitor, User } from 'grommet-icons';
 
 // stickers list
 export const Row1 = [
-    {
-      img: '/img/StickerPage/metalhand.png',
-      icon: <Monitor size="small" />,
-      backgroundColor: 'background-contrast',
-    },
-    {
-      img: '/img/StickerPage/gremlinrockin.png',
-      icon: <Monitor size="small" />,
-      backgroundColor: '#FFBC44',
-    },
-    {
-      img: '/img/StickerPage/grommet-sticker.png',
-      icon: <Monitor size="small" />,
-      backgroundColor: '#FFFFFF',
-    },
-    { img: '/img/StickerPage/it-monster.png', icon: <User size="small" /> },
-    // put hand twice for now
-    {
-      img: '/img/StickerPage/metalhand.png',
-      icon: <Monitor size="small" />,
-      backgroundColor: 'background-contrast',
-    },
-    // put back in ezmeral picture
-  //   { 
+  {
+    img: '/img/StickerPage/metalhand.png',
+    icon: <Monitor size="small" />,
+    backgroundColor: 'background-contrast',
+  },
+  {
+    img: '/img/StickerPage/gremlinrockin.png',
+    icon: <Monitor size="small" />,
+    backgroundColor: '#FFBC44',
+  },
+  {
+    img: '/img/StickerPage/grommet-sticker.png',
+    icon: <Monitor size="small" />,
+    backgroundColor: '#FFFFFF',
+  },
+  {
+    img: '/img/StickerPage/it-monster.png',
+    icon: <User size="small" />,
+  },
+  // put hand twice for now
+  {
+    img: '/img/StickerPage/metalhand.png',
+    icon: <Monitor size="small" />,
+    backgroundColor: 'background-contrast',
+  },
+  // put back in ezmeral picture
+  //   {
   //     // img: '/img/StickerPage/ezmeral.png',
-  //     // icon: <User size="small" />, 
+  //     // icon: <User size="small" />,
   // },
-  ];
+];
 
-  export const Row2 = [
-    { img: '/img/StickerPage/hpedevlogo.png', icon: <Monitor size="small" /> },
-    { img: '/img/StickerPage/gremlin.png', icon: <User size="small" /> },
-  ];
-  
- export const Row6 = [
-    {
-      img: '/img/StickerPage/ResearchDesignDev.png',
-      backgroundColor: 'white',
-      icon: <Monitor size="small" />,
-    },
-    { 
-      img: '/img/StickerPage/designedwlove.png', 
-      icon: <Monitor size="small" />,
-    },
-  ];
+export const Row2 = [
+  {
+    img: '/img/StickerPage/06-HackShack.png',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+  {
+    img: '/img/StickerPage/hpedevlogo.png',
+    icon: <Monitor size="small" />,
+  },
+  {
+    img: '/img/StickerPage/gremlin.png',
+    icon: <User size="small" />,
+  },
+  {
+    backgroundImage: 'url(/img/StickerPage/grommet-icon.png)',
+    download: '/img/StickerPage/grommet-icon.png',
+    icon: <Monitor size="small" />,
+  },
+];
+
+export const Row3 = [
+  {
+    backgroundColor: '#82FFF2',
+    icon: <User size="small" />,
+    img: '/img/StickerPage/gremlinhat.png',
+  },
+  {
+    // Take out backgroundColor and put back background Image
+    // backgroundImage: 'url(/img/StickerPage/ezmeralbackground.png)',
+    backgroundColor: 'background-contrast',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+  {
+    img: '/img/StickerPage/letshackshack.png',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+];
+
+export const Row4 = [
+  {
+    icon: <User size="small" />,
+    img: '/img/StickerPage/devlogo.png',
+  },
+  {
+    backgroundImage: 'url(/img/StickerPage/2gremlins.png)',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+  {
+    backgroundImage: 'url(/img/StickerPage/hack-shack-house.png)',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+];
+
+export const Row5 = [
+  {
+    backgroundColor: 'black',
+    icon: <Monitor size="small" />,
+    img: '/img/StickerPage/attack-marquee.svg',
+    size: 'large',
+  },
+  {
+    icon: <User size="small" />,
+    img: '/img/StickerPage/dev-thumb.png',
+  },
+  {
+    backgroundImage: 'url(/img/StickerPage/design.png)',
+    icon: <Monitor size="small" />,
+  },
+  {
+    backgroundImage: 'url(/img/StickerPage/heart.png)',
+    icon: <User size="small" />,
+  },
+];
+
+export const Row6 = [
+  {
+    img: '/img/StickerPage/itmonsterattack.png',
+    icon: <Monitor size="small" />,
+  },
+  {
+    img: '/img/StickerPage/ResearchDesignDev.png',
+    backgroundColor: 'white',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+  {
+    img: '/img/StickerPage/designedwlove.png',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+];
+
+// Need extra rows for mobile view
+export const Row7 = [
+  {
+    backgroundImage: 'url(/img/StickerPage/grommet-icon.png)',
+    download: '/img/StickerPage/grommet-icon.png',
+    icon: <Monitor size="small" />,
+  },
+  {
+    backgroundImage: 'url(/img/StickerPage/heart.png)',
+    icon: <User size="small" />,
+  },
+  {
+    img: '/img/StickerPage/designedwlove.png',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+];
+
+export const Row8 = [
+  {
+    img: '/img/StickerPage/letshackshack.png',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+  {
+    backgroundImage: 'url(/img/StickerPage/hack-shack-house.png)',
+    icon: <Monitor size="small" />,
+    size: 'large',
+  },
+];
