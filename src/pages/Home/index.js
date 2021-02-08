012e72919ca5603918618be71a2c8f49d4fad499
pramod sareddy <reddypramod85>
2021-02-08 15:20:32 -0600

    fix munch and learn desc

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -127,7 +127,7 @@ const Cards = ({ size }) => (
     <Card
       logo="/img/MunchandLearn.svg"
       title="Introducing HPE DEV Munch & Learn series"
-      desc="Session 2: Explore Containerization and MLOps"
+      desc="Session 2: Explore Containerization and MLOps."
       link=" https://hpe.zoom.us/meeting/register/tJYkdequqTwsE9LuPAgPDbV-mf1V7jq23Mxj"
       background="rgba(0, 86, 122, 0.8);"
       label="Register Now!"
