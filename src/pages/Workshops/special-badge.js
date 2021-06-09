b0197f5f507080e1c15c76d583922df0f2ae8cc1
Johnathan Giang <jayhgiang@gmail.com>
2021-06-09 07:48:18 -0700

    Remove helmet import in special-badge page

--- src/pages/Workshops/special-badge.js
+++ src/pages/Workshops/special-badge.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import WorkshopsTemplate from './template';
-import { Helmet } from 'react-helmet';
 
 const SpecialBadge = props => {
   return <WorkshopsTemplate  {...props} />
